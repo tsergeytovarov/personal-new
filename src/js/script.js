7b1c108cc43c73b3ed296d3a3aba969a529d2e2b
Сергей Попов <tovarov.piter@gmail.com>
2016-02-09 13:22:31 +0300

    открытие модального окна

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+(function() {
+  var menuOpen = document.querySelector('.hamburger');
+  var modalMenu = document.querySelector('.menu-overlay');
+  menuOpen.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    if (menuOpen.classList.contains('active')) {
+      menuOpen.classList.remove('active');
+      modalMenu.classList.remove('active');
+    } else {
+      menuOpen.classList.add('active');
+      modalMenu.classList.add('active');
+    }
+  });
+})();
